e8fa3ce11f060786de4dab901a1de1726f1c233d
Nino Kelehsashvili <20074819@mail.wit.ie>
2022-08-22 22:26:58 +0100

    Exercise 1 completed.

--- src/components/exercises/01_staticComponent.js
+++ src/components/exercises/01_staticComponent.js
@@ -3,7 +3,30 @@ import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 const Demo = () => {
   return (
-      <h1>TODO</h1>
+    <div>
+    <h2>Computer Science - Modules table</h2>
+    <table className="table table-bordered">
+      <thead>
+        <tr>
+          <th>Module</th>
+          <th>Lectures</th>
+          <th>Practicals</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>DevOps</td>
+          <td>2</td>
+          <td>3</td>
+        </tr>
+        <tr>
+          <td>Enterprise Web Dev</td>
+          <td>1</td>
+          <td>4</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
   );
 };
 
