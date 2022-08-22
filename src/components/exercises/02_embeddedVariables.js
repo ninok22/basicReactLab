29c4b77a0ebfabe300b16c2485f2bb853c34b226
Nino Kelehsashvili <20074819@mail.wit.ie>
2022-08-22 22:48:20 +0100

    Exercise 2 completed.

--- src/components/exercises/02_embeddedVariables.js
+++ src/components/exercises/02_embeddedVariables.js
@@ -3,8 +3,44 @@ import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 
 const Demo = () => {
+  const course = "Computer Science - Modules table."
+  const modules = [
+    {
+      name: "DevOps",
+      lectures: 2,
+      practicals: 3
+    },
+    {
+      name: "Web App",
+      lectures: 1,
+      practicals: 4
+    }
+  ]
   return (
-      <h1>TODO</h1>
+    <div>
+    <h2>{course}</h2>
+    <table className="table table-bordered">
+      <thead>
+        <tr>
+          <td>Modules</td>
+          <td>Lectures</td>
+          <td>Practicals</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{modules[0].name}</td>
+          <td>{modules[0].lectures}</td>
+          <td>{modules[0].practicals}</td>
+        </tr>
+        <tr>
+          <td>{modules[1].name}</td>
+          <td>{modules[1].lectures}</td>
+          <td>{modules[1].practicals}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
   );
 };
 
